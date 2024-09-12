1298d6b02f86d66675278e63e5a3104071f7668f
Dima <144374719+Dima-Chuchman@users.noreply.github.com>
2024-09-12 18:12:00 +0300

    Adding Classwork 1
    Classwork(1) 13.09.2024
    Tasks: 1.1 - 1.9

---
+++ Task5.cpp
@@ -0,0 +1,23 @@
+/*
+
+Task 1.5
+
+Дано дійсне число x. Користуючись лише операцією множення, отримати x^15 за 5 операцій.
+
+*/
+
+//#include <stdio.h>
+
+//int main() {
+//
+//	double x;
+//	scanf_s("%lf", &x);
+//
+//	double x2 = x * x;
+//	double x4 = x2 * x2;
+//	double x8 = x4 * x4;
+//	double x12 = x8 * x4;
+//	double x15 = x12 * x2 * x;
+//
+//	printf("x^15 = %.2lf\n", x15);
+//}

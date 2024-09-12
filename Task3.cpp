46cf474eeedceed02b16708dc9336401adb817e4
Dima <144374719+Dima-Chuchman@users.noreply.github.com>
2024-09-12 18:05:45 +0300

    Adding Classwork 1
    Classwork 13.09.2024
    Tasks: 1.1 - 1.9

---
+++ Task3.cpp
@@ -0,0 +1,26 @@
+/*
+
+Task 1.3
+
+Вивести на екран:
+
+- a - a - a
+a | a | a
+- a - a - a
+
+де а - введена з клавіатури цифра
+
+*/
+
+//#include <stdio.h>4
+//
+//int main() {
+//
+//    char a;
+//
+//    scanf_s("%c", &a);
+//
+//    printf("- %c - %c - %c\n", a, a, a);
+//    printf("%c | %c | %c\n", a, a, a);
+//    printf("- %c - %c - %c\n", a, a, a);
+//}
